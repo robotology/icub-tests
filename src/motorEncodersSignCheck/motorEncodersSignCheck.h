036975a367ce084bd9cc87f71157526f859cba6a
Valentina Gaggero <valentina.gaggero@iit.it>
2015-12-22 13:53:06 +0100

    added doc for motorEncoderSignCheck

--- src/motorEncodersSignCheck/motorEncodersSignCheck.h
+++ src/motorEncodersSignCheck/motorEncodersSignCheck.h
@@ -22,8 +22,30 @@
 /**
 * \ingroup icub-tests
 * This tests checks if the motor encoder readings increase when positive pwm is applayed to motor.
+* This test helps you to check if RotorEncoderResolution parameter in robot's configuration files has
+* correct sign.
+* For each joint given in test's configuration file
+* The test sets one joint per time in Open Loop control mode; then applies positive pwm starting with value defined in parameter "pwmStart"
+* and increments pwm with step defined in parameter "pwmStep" until motors doesn't move of Posthreshold deg at least.
 *
-* ...work in progress...
+*
+* Note: This test uses RTF::YARP::jointsPosMotion class, a class for reduce time in developing test.
+*
+*
+*  Accepts the following parameters:
+* | Parameter name     | Type   | Units | Default Value | Required | Description | Notes |
+* |:------------------:|:------:|:-----:|:-------------:|:--------:|:-----------:|:-----:|
+* | robot              | string | -     | -             | Yes      | The name of the robot.     | e.g. icub |
+* | name               | string | -     | -             | No       | The name of test. | e.g. motEncSignCheck_Head |
+* | part               | string | -     | -             | Yes      | The name of trhe robot part. | e.g. head |
+* | joints             | vector of ints | -     |     - | Yes      | List of joints to be tested | |
+* | home               | vector of doubles of size joints  | deg   | - | Yes | The home position for each joint. It should be distant from joint's limits | |
+* | speed              | vector of doubles of size joints  | deg/s | - | Yes | The reference speed used during the movement  | |
+* | pwmStart           | vector of doubles of size joints  | -     | - | Yes | The starting pwm applied to joint | |
+* | pwmStep            | vector of doubles of size joints  | -     | - | Yes | The increment of pwm per time | |
+* | pwmMax             | vector of doubles of size joints  | -     | - | Yes | The max pwm applicable | |
+* | Posthreshold       | vector of doubles of size joints  | deg   | 5 | No  | The minumum movement to check if motor position increases | |
+* | commandDelay       | vector of doubles of size joints  | deg   | 0.1 | No  | The delay between two SetRefOpenLooop commands consecutive | |
 *
 */
 class MotorEncodersSignCheck : public YarpTestCase {
