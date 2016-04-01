bd3a3829d91b0ad6595ea7cb7e7ef38daf202ff6
Valentina Gaggero <valentina.gaggero@iit.it>
2016-04-01 14:30:03 +0200

    updated octave script for encoderConsistency test

--- suits/contexts/scripts/oneFile_plot.m
+++ suits/contexts/scripts/oneFile_plot.m
@@ -1,12 +1,16 @@
 #define functio to plot one file
-function oneFile_plot(filename, titleStr)
-#if (nargin != 1)
-#    error ("usage: one_plot(filename)");
-#endif
+function oneFile_plot(filename, titleStr, numofjoint)
+
 data = load(filename);
-for i= 1:4
-    subplot(4, 1, i);
-    plot(data(:, 0+i), "r", data(:,4+i), "b");
+for i= 1:1:numofjoint
+    if(numofjoint>4)
+        subplot(3, 2, i, "align");
+    else
+        subplot(numofjoint, 1, i, "align");
+    endif
+    printf("index %d\n", i);
+    plot(data(:, 0+i), "r", data(:,numofjoint+i), "b");
+    refresh();
     if(i==1)
     title(titleStr);
     endif
