22a8606cc4af59be62a04b4a5aa3ba48af6d3194
Ugo Pattacini <ugo.pattacini@iit.it>
2016-11-17 15:20:34 +0100

    added test for simple p2p cartesian movement

---
+++ src/cartesian-control/README.md
@@ -0,0 +1,9 @@
+iCub cartesian-control
+======================
+
+Set of tests meant to verify the functionality of the [**Cartesian Control**](http://wiki.icub.org/iCub/main/dox/html/icub_cartesian_interface.html)
+component.
+
+### Available tests
+
+- [`simple-p2p-movement`](https://github.com/robotology/icub-tests/blob/master/src/cartesian-control/simple-p2p-movement.cpp).
---
+++ src/cartesian-control/simple-p2p-movement/CartesianControlSimpleP2pMovementTest.h
@@ -0,0 +1,38 @@
+/* 
+ * Copyright (C) 2016 iCub Facility - Istituto Italiano di Tecnologia
+ * Author: Ugo Pattacini
+ * email:  ugo.pattacini@iit.it
+ * Permission is granted to copy, distribute, and/or modify this program
+ * under the terms of the GNU General Public License, version 2 or any
+ * later version published by the Free Software Foundation.
+ *
+ * A copy of the license can be found at
+ * http://www.robotcub.org/icub/license/gpl.txt
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details
+*/
+
+#ifndef _CARTESIANCONTROLSIMPLEP2PMOVEMENT_H_
+#define _CARTESIANCONTROLSIMPLEP2PMOVEMENT_H_
+
+#include <rtf/yarp/YarpTestCase.h>
+#include <yarp/os/Property.h>
+#include <yarp/dev/PolyDriver.h>
+
+class CartesianControlSimpleP2pMovementTest : public YarpTestCase
+{
+    yarp::dev::PolyDriver driver;
+
+public:
+    CartesianControlSimpleP2pMovementTest();
+    virtual ~CartesianControlSimpleP2pMovementTest();
+    virtual bool setup(yarp::os::Property& property);
+    virtual void tearDown();
+    virtual void run();
+};
+
+#endif
+
