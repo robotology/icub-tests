a80e7982292bdc18973cd0477349af7bcae60507
tumme <davide.tome@iit.it>
2022-02-17 17:24:54 +0100

    matrix correctly reconstructed - to be tested on the robot

--- src/motorEncoders-consistency/get-j2m-matrix/main.cpp
+++ src/motorEncoders-consistency/get-j2m-matrix/main.cpp
@@ -17,28 +17,13 @@
 #include <yarp/os/ResourceFinder.h>
 #include <yarp/os/Value.h>
 #include <yarp/dev/PolyDriver.h>
-#include <yarp/dev/IPositionControl.h>
-#include <yarp/dev/IPidControl.h>
-#include <yarp/dev/IControlMode.h>
-#include <yarp/dev/IEncoders.h>
 #include <yarp/dev/IRemoteVariables.h>
 #include <yarp/sig/Matrix.h>
-#include <algorithm>
-#include <cstdlib>
-#include <fstream>
-#include <iostream>
+
 
 using namespace std;
 using namespace yarp::os;
 using namespace yarp::dev;
-
-struct DataExperiment {
-    double t;
-    double pid_ref;
-    double pid_out;
-    double enc;
-};
-
 
 
 int main(int argc, char * argv[])
@@ -58,29 +43,18 @@ int main(int argc, char * argv[])
     auto T = rf.check("T", Value(2.)).asDouble();
 
     PolyDriver m_driver;
-    IPositionControl* iPos{ nullptr };
-    IPidControl* iPid{ nullptr };
-    IControlMode* iCm{ nullptr };
-    IEncoders* iEnc{ nullptr };
+   
     IRemoteVariables* iVars{ nullptr };
-
-    std::ofstream file;
-
-    file.open("output.log");
-
-    std::vector<DataExperiment> data_vec;
-    data_vec.reserve(1000);
 
     Property conf;
     conf.put("device", "remote_controlboard");
-    conf.put("remote", "/icub/left_leg");
+    conf.put("remote", "/icub/left_arm");
     conf.put("local", "/logger");
     if (!m_driver.open(conf)) {
         yError() << "Failed to open";
         return EXIT_FAILURE;
     }
-    if (!(m_driver.view(iPos) && m_driver.view(iPid) &&
-          m_driver.view(iCm) && m_driver.view(iEnc)  && m_driver.view(iVars))) {
+    if (!(m_driver.view(iVars))) {
         m_driver.close();
         yError() << "Failed to view interfaces";
         return EXIT_FAILURE;
@@ -88,58 +62,57 @@ int main(int argc, char * argv[])
 
     yarp::os::Bottle b,b1;
 
+    iVars->getRemoteVariable("kinematic_mj", b);
 
-    iVars->getRemoteVariablesList(&b1);
-    // yInfo() << "list variables : " << b1.toString();
-
-    iVars->getRemoteVariable("kinematic_j2m", b);
-    // iVars->getRemoteVariable("gearbox_M2J", b1);
-
-    yInfo() << "kinematic_j2m : " << b.toString() << " lenght : " << b.size();
     auto bb = b.get(1);
 
-    int matrix_size = 6;
+    int matrix_size = 4;
     string s;
-    yarp::sig::Matrix matrix, matrix1;
-    
+    yarp::sig::Matrix matrix;
+
     matrix.resize(matrix_size,matrix_size);
     matrix.eye();
+    int njoints [4];
 
-    for (int i=0; i< (matrix_size*matrix_size); i++)
-            {
-                //matrix.data()[i]= i;
-                yDebug() << "TUMME matrix: " << matrix.data()[i];
-            }
-
-    char buff [500];
-    sprintf(buff,"Matrix:\n %s \n", matrix.toString().c_str());
-
-    yDebug() << matrix.toString();
-
-    
     for(int i=0 ; i< b.size() ; i++)
     {
-       
-        s += " " + b.get(i).toString();
+        Bottle bv;
+        bv.fromString(b.get(i).toString());
+        njoints[i] = sqrt(bv.size());
+        
+       int ele = 0;
+       if(i==0) {
+           for (int r=0; r < njoints[i]; r++) 
+            {
+                for (int c=0; c < njoints[i]; c++) 
+                {
+                    matrix(r,c) = bv.get(ele).asFloat64();
+                    ele++;
+                }
+            }
 
+       }  
+       else{
+           for (int r=0; r < njoints[i]; r++) 
+            {
+                for (int c=0; c < njoints[i]; c++) 
+                {
+                    int jntprev = 0;
+                    for (int j=0; j < i; j++) jntprev += njoints[j];
+                    if(!jntprev > matrix_size)  matrix(r+jntprev,c+jntprev) = bv.get(ele).asFloat64();
+                    ele++;
+                }
+            }
+       }
+
+      
     }
 
-    Bottle bv;
-    bv.fromString(s);
+    yDebug() << " \n" << matrix.toString();
 
-    yDebug() << bv.toString();
-
-    yDebug() << bv.size();
-
-    yDebug() << bv.get(0).asFloat64();
-
- 
-   
-    
     m_driver.close();
-    file.close();
 
-    yInfo()<<"Main returning...";
+    yInfo() << "Main returning...";
     return EXIT_SUCCESS;
     
 }
