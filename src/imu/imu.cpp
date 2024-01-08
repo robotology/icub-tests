efe202fe8e9caae5e3813c04e900d18e44e43652
Martina Gloria <martina.gloria@iit.it>
2024-01-08 17:21:50 +0100

    Add yarpscope visualization of IMU data from port

--- src/imu/imu.cpp
+++ src/imu/imu.cpp
@@ -5,8 +5,8 @@
 #include <robottestingframework/dll/Plugin.h>
 
 #include <yarp/os/Property.h>
+#include <yarp/os/Stamp.h>
 
-#include <eigen3/Eigen/Dense>
 #include "imu.h"
 
 using namespace robottestingframework;
@@ -32,60 +32,68 @@ bool Imu::setup(yarp::os::Property& property) {
 
     ROBOTTESTINGFRAMEWORK_TEST_REPORT("Running IMU test on "+robotName+" for "+partName);
 
-    yarp::os::Property options;
-    options.put("device", "multipleanalogsensorsclient");
-    options.put("remote", "/"+robotName+"/"+partName+"/inertials");
-    options.put("local", "/imuTest/"+robotName+"/"+partName);
+    yarp::os::Property options1;
+    options1.put("device", "multipleanalogsensorsclient");
+    options1.put("remote", "/"+robotName+"/"+partName+"/inertials");
+    options1.put("local", "/imuTest/"+robotName+"/"+partName);
 
-    driver = new yarp::dev::PolyDriver(options);
-    ROBOTTESTINGFRAMEWORK_ASSERT_ERROR_IF_FALSE(driver->isValid(), "Device driver cannot be opened");
-    ROBOTTESTINGFRAMEWORK_ASSERT_ERROR_IF_FALSE(driver->view(iorientation), "Unable to open orientation interface");
+    yarp::os::Property options2;
+    options2.put("device", "remote_controlboard");
+    options2.put("remote", "/"+robotName+"/alljoints");
+    options2.put("local", "/local");
 
-    size_t m_imuSensorIndex = 0;
-    std::string name;
+    driver1 = new yarp::dev::PolyDriver(options1);
+    driver2 = new yarp::dev::PolyDriver(options2);
+    ROBOTTESTINGFRAMEWORK_ASSERT_ERROR_IF_FALSE(driver1->isValid(), "Device driver cannot be opened");
+    ROBOTTESTINGFRAMEWORK_ASSERT_ERROR_IF_FALSE(driver1->view(iorientation), "Unable to open orientation interface");
+    ROBOTTESTINGFRAMEWORK_ASSERT_ERROR_IF_FALSE(driver2->isValid(), "Device driver cannot be opened");
+    ROBOTTESTINGFRAMEWORK_ASSERT_ERROR_IF_FALSE(driver2->view(ipos), "Unable to open position control interface");
+    ROBOTTESTINGFRAMEWORK_ASSERT_ERROR_IF_FALSE(driver2->view(ienc), "Unable to open encoder interface");
 
-    auto nbSensors = iorientation->getNrOfOrientationSensors();	
-    std::cout << nbSensors << std::endl;
     yarp::dev::MAS_status status;
-    status = iorientation->getOrientationSensorStatus(m_imuSensorIndex);
+    status = iorientation->getOrientationSensorStatus(iorientation->getNrOfOrientationSensors()-1);
     std::cout << "MAS_status: " << status << std::endl; //MAS_status = 4 -> The sensor is read through the network, and the device is waiting to receive the first measurement. 
 
     outputPort.open("/test-imu/out");
-
-
-    // while(true);
-    // {
-    //     out.addFloat64(rpyValues[0]);
-    //     outputPort.write(&out);
-    //     std::cout << out.toString().c_str() << std::endl;
-
-    //     yarp::os::Time::delay(0.1);
-
-    // }
+    ROBOTTESTINGFRAMEWORK_TEST_REPORT("Opening port "+outputPort.getName()+"...");
 
     //------------------------------------------------------------------------
-    bool ok = model.loadModelFromFile(modelPath.c_str());
-    ROBOTTESTINGFRAMEWORK_ASSERT_ERROR_IF_FALSE(ok, Asserter::format("Cannot load model %s", modelPath.c_str()));
+    ROBOTTESTINGFRAMEWORK_ASSERT_ERROR_IF_FALSE(model.loadModelFromFile(modelPath.c_str()), Asserter::format("Cannot load model %s", modelPath.c_str()));
     
     kinDynComp.loadRobotModel(model.model());
     std::cout << "The loaded model has " << kinDynComp.model().getNrOfDOFs() << " internal degrees of freedom and " << kinDynComp.model().getNrOfLinks() << " links." << std::endl;
-    std::array<double, 3> baseLinkOrientationDeg {0.0, 0.0, 0.0};
-    std::array<double, 3> baseLinkOrientationRad;
+    // for(auto i = 0; i < kinDynComp.model().getNrOfDOFs(); i++)
+    // {
+    //     std::cout << "Joint " << i << " is " << kinDynComp.model().getJointName(i) << std::endl;
+    // }
+
+    iDynTree::Vector3 baseLinkOrientationDeg;
+    iDynTree::Vector3 baseLinkOrientationRad;
+    baseLinkOrientationDeg.zero();
+
     for (auto i : baseLinkOrientationDeg)
     {
-        baseLinkOrientationRad[i] = baseLinkOrientationDeg[i] * M_PI/180;
+        baseLinkOrientationRad[i] = iDynTree::deg2rad(baseLinkOrientationDeg[i]);
     }
 
-    auto baseLinkOrientation = iDynTree::Rotation::RPY(baseLinkOrientationRad[0], baseLinkOrientationRad[1], baseLinkOrientationRad[2]);
-    
+    baseLinkOrientation = iDynTree::Rotation::RPY(baseLinkOrientationRad[0], baseLinkOrientationRad[1], baseLinkOrientationRad[2]);
     iDynTree::Transform I_T_base(baseLinkOrientation, iDynTree::Position::Zero());
     iDynTree::Twist baseVelocity = iDynTree::Twist::Zero();
+
     unsigned int dofs = kinDynComp.model().getNrOfDOFs();
-    iDynTree::VectorDynSize s(dofs), ds(dofs);
+    s.resize(dofs);
+    ds.resize(dofs);
 
-    std::cout << ds.toString() << std::endl;
+    ROBOTTESTINGFRAMEWORK_ASSERT_ERROR_IF_FALSE(ienc->getAxes(&axes), "Cannot get number of controlled axes");
+    for(auto i = 0; i < axes; i++)
+    {
+        ROBOTTESTINGFRAMEWORK_ASSERT_ERROR_IF_FALSE(ienc->getEncoder(i, &positions), "Cannot get joint positions");
+        ROBOTTESTINGFRAMEWORK_ASSERT_ERROR_IF_FALSE(ienc->getEncoderSpeed(i, &velocities), "Cannot get joint positions");
 
-    iDynTree::Vector3 gravity;
+        s.setVal(i, positions);
+        ds.setVal(i, velocities);
+    }
+
     gravity.zero();
     gravity(2) = -9.81;
 
@@ -96,18 +104,24 @@ bool Imu::setup(yarp::os::Property& property) {
         ds,
         gravity
     );
-    
     //-----------------------------------------------------------------------------
     return true;
 }
 
 void Imu::tearDown() {
+    outputPort.interrupt();
     outputPort.close();
 
-    if(driver)
+    if(driver1)
     {
-        delete driver;
-        driver = 0;
+        delete driver1;
+        driver1 = 0;
+    }
+
+    if(driver2)
+    {
+        delete driver2;
+        driver2 = 0;
     }
 }
 
@@ -122,36 +136,72 @@ void Imu::run() {
     ROBOTTESTINGFRAMEWORK_ASSERT_ERROR_IF_FALSE(iorientation->getOrientationSensorFrameName(0, imuFrameName), "Unable to get sensor frame name");
     ROBOTTESTINGFRAMEWORK_ASSERT_ERROR_IF_FALSE(iorientation->getOrientationSensorMeasureAsRollPitchYaw(0, rpyValues, timestamp), "Unable to obtain rpy measurements.");
 
-    std::cout << "IMU frame name is: " << imuFrameName << std::endl;
-    std::cout << "IMU sensor name is: " << imuSensorName << std::endl;
+    // std::cout << "IMU frame name is: " << imuFrameName << std::endl;
+    // std::cout << "IMU sensor name is: " << imuSensorName << std::endl;
 
-    yarp::os::Bottle& out = outputPort.prepare();
-    out.clear();
     auto count = 0;
-    while(count < 100)
-    {
-        iorientation->getOrientationSensorMeasureAsRollPitchYaw(0, rpyValues, timestamp);
-        out.addFloat64(rpyValues[0]);
-        out.addFloat64(rpyValues[1]);
-        out.addFloat64(rpyValues[2]);
+    iDynTree::Twist baseVelocity = iDynTree::Twist::Zero();
+    iDynTree::Transform I_T_base(baseLinkOrientation, iDynTree::Position::Zero());
 
-        // std::cout << out.toString().c_str() << std::endl;
-        outputPort.write();
+    while(true)
+    {
+        iorientation->getOrientationSensorMeasureAsRollPitchYaw(0, rpyValues, timestamp); //rpy in deg
+        sendData(rpyValues);
+
         count++;
-        yarp::os::Time::delay(0.01);
+        // yarp::os::Time::delay(0.01);
 
         //-----------------------------------------------------------
-        iDynTree::Rotation I_R_FK = kinDynComp.getWorldTransform(frameName).getRotation();
+        iDynTree::Rotation I_R_FK = kinDynComp.getWorldTransform(frameName).getRotation(); //compute the orientation of the frame where the IMU is attached 
         iDynTree::Rotation I_R_I_IMU = (I_R_FK * (iDynTree::Rotation::RPY(rpyValues[0], rpyValues[1], rpyValues[2])).inverse()); 
-        
+
+        std::cout << "From FK: " << I_R_I_IMU.asRPY().toString() << std::endl;
+
+        for (auto i = 0; i < axes; i++)
+        {
+            ROBOTTESTINGFRAMEWORK_ASSERT_ERROR_IF_FALSE(ienc->getEncoder(i, &positions), "Cannot get joint positions");
+            ROBOTTESTINGFRAMEWORK_ASSERT_ERROR_IF_FALSE(ienc->getEncoderSpeed(i, &velocities), "Cannot get joint positions");
+            
+            s.setVal(i, positions);
+            ds.setVal(i, velocities);
+        }  
+
+        // std::cout << "s: " << s.toString() << std::endl;
+
+        kinDynComp.setRobotState(
+        I_T_base,
+        s,
+        baseVelocity,
+        ds,
+        gravity);
+
         iDynTree::Rotation expectedImuSignal = kinDynComp.getWorldTransform(frameName).getRotation();
         iDynTree::Rotation imuSignal = (I_R_I_IMU * iDynTree::Rotation::RPY(rpyValues[0], rpyValues[1], rpyValues[2]));
 
         auto error = (expectedImuSignal * imuSignal.inverse()).log();
+        // std::cout << "From FK: " << imuSignal.asRPY().toString() << std::endl;
 
         //-----------------------------------------------------------
-
-        out.clear();
     }
 
+}
+
+bool Imu::sendData(yarp::sig::Vector rpy)
+{
+    yarp::os::Bottle& out = outputPort.prepare();
+    static yarp::os::Stamp stamp;
+
+    stamp.update();
+    outputPort.setEnvelope(stamp);
+    out.clear();
+
+    out.addFloat64(iDynTree::deg2rad(rpy[0]));
+    out.addFloat64(iDynTree::deg2rad(rpy[1]));
+    out.addFloat64(iDynTree::deg2rad(rpy[2]));
+
+    out.addFloat64(stamp.getTime());
+
+    outputPort.write();    
+    
+    return true;
 }
--- src/imu/imu.h
+++ src/imu/imu.h
@@ -3,42 +3,52 @@
 
 #include <yarp/dev/PolyDriver.h>
 #include <yarp/dev/MultipleAnalogSensorsInterfaces.h>
+#include <yarp/dev/IPositionControl.h>
+#include <yarp/dev/IEncoders.h>
 #include <yarp/robottestingframework/TestCase.h>
 
 #include <iDynTree/KinDynComputations.h>
 #include <iDynTree/ModelLoader.h>
 #include <iDynTree/Model.h>
 
-struct SensorInfo {
-    std::string sensorName{" "};
-    std::string frameName{" "};
-    iDynTree::Transform transform{iDynTree::Transform::Identity()};
-};
-
 class Imu : public yarp::robottestingframework::TestCase {
-public:
-    Imu();
-    virtual ~Imu();
-    virtual bool setup(yarp::os::Property& property);
-    virtual void tearDown();
-    virtual void run();
+    public:
+        Imu();
+        virtual ~Imu();
+        virtual bool setup(yarp::os::Property& property);
+        virtual void tearDown();
+        virtual void run();
 
-private:
-    std::string robotName;
-    std::string partName;
-    std::string portName;
-    std::string modelPath;
-    std::string frameName;
-    std::string sensorName;
+    private:
+        std::string robotName;
+        std::string partName;
+        std::string portName;
+        std::string modelPath;
+        std::string frameName;
+        std::string sensorName;
 
-    yarp::dev::PolyDriver *driver;
-    yarp::dev::IOrientationSensors* iorientation;
+        yarp::dev::PolyDriver *driver1;
+        yarp::dev::PolyDriver *driver2;
+        yarp::dev::IOrientationSensors* iorientation;
+        yarp::dev::IPositionControl* ipos;
+        yarp::dev::IEncoders* ienc;
 
-    yarp::os::BufferedPort <yarp::os::Bottle> outputPort;
-    yarp::sig::Vector rpyValues;
+        yarp::os::BufferedPort <yarp::os::Bottle> outputPort;
+        yarp::sig::Vector rpyValues;
+        double positions;
+        double velocities;
+        int axes;
 
-    iDynTree::ModelLoader model;
-    iDynTree::KinDynComputations kinDynComp;
+        iDynTree::ModelLoader model;
+        iDynTree::KinDynComputations kinDynComp;
 
-};
+        iDynTree::VectorDynSize s;
+        iDynTree::VectorDynSize ds;
+        iDynTree::Vector3 gravity;
+        iDynTree::Rotation baseLinkOrientation;
+
+        bool sendData(yarp::sig::Vector rpy);
+
+    };
+
 #endif //_IMU_H_
