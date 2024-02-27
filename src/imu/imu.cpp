d35471a404daddd71fe62e6f4c6fd1e018e73154
Martina Gloria <martina.gloria@iit.it>
2024-02-27 14:28:00 +0100

    Test all the IMUs while doing a sequence of movements

--- src/imu/imu.cpp
+++ src/imu/imu.cpp
@@ -29,6 +29,7 @@ bool Imu::setup(yarp::os::Property& property)
     ROBOTTESTINGFRAMEWORK_ASSERT_ERROR_IF_FALSE(property.check("axesNames"), "Please, provide the controlled joints name.");
     ROBOTTESTINGFRAMEWORK_ASSERT_ERROR_IF_FALSE(property.check("model"), "Please, provide the urdf model path.");
     ROBOTTESTINGFRAMEWORK_ASSERT_ERROR_IF_FALSE(property.check("meanError"), "Please, provide the threshold error.");
+    ROBOTTESTINGFRAMEWORK_ASSERT_ERROR_IF_FALSE(property.check("sensorsList"), "Please, provide the list of the sensors you want to check or 'all' if you want to test all the IMUs.");
     
     robotName = property.find("robot").asString(); // robot name
     portName = property.find("port").asString(); // name of the port from which the data are streamed
@@ -38,25 +39,6 @@ bool Imu::setup(yarp::os::Property& property)
     std::string modelAbsolutePath = rf.findFileByName(modelName);
     
     ROBOTTESTINGFRAMEWORK_TEST_REPORT("Running IMU test on "+robotName+"...");
-    
-    yarp::os::Bottle *sensorsList;
-    sensorsList = property.find("sensorsList").asList();
-    for(int sensor = 0; sensor < sensorsList->size(); sensor++)
-    {
-        yarp::os::Bottle &sensors = property.findGroup(sensorsList->get(sensor).asString());
-
-        yarp::os::Bottle moveJoints;
-        yarp::os::Bottle *inputMoveJoints;
-        inputMoveJoints = sensors.find("moveJoints").asList();
-
-        for (int movJoint = 0; movJoint < inputMoveJoints->size(); movJoint++)
-        {
-            moveJoints.addString(inputMoveJoints->get(movJoint).asString());
-        }
-
-        sensorMap[sensorsList->get(sensor).asString()] = moveJoints;
-        moveJoints.clear();
-    }
 
     yarp::os::Property controlBoardOptions;
     controlBoardOptions.put("device", "remotecontrolboardremapper");
@@ -82,13 +64,12 @@ bool Imu::setup(yarp::os::Property& property)
     controlBoardOptions.put("remoteControlBoards", remoteControlBoards.get(0));
     controlBoardOptions.put("localPortPrefix", "/test");
     controlBoardOptions.put("axesNames", axesNames.get(0));
-
+    
     ROBOTTESTINGFRAMEWORK_ASSERT_ERROR_IF_FALSE(controlBoardDriver.open(controlBoardOptions), "Unable to open the controlBoard driver");
     ROBOTTESTINGFRAMEWORK_ASSERT_ERROR_IF_FALSE(controlBoardDriver.isValid(), "Device driver cannot be opened");
     ROBOTTESTINGFRAMEWORK_ASSERT_ERROR_IF_FALSE(controlBoardDriver.view(ipos), "Unable to open position control interface");
     ROBOTTESTINGFRAMEWORK_ASSERT_ERROR_IF_FALSE(controlBoardDriver.view(ienc), "Unable to open encoder interface");
     ROBOTTESTINGFRAMEWORK_ASSERT_ERROR_IF_FALSE(controlBoardDriver.view(iaxes), "Unable to open axes interface");
-    ROBOTTESTINGFRAMEWORK_ASSERT_ERROR_IF_FALSE(controlBoardDriver.view(ilim), "Unable to open limits interface");
 
     ROBOTTESTINGFRAMEWORK_ASSERT_ERROR_IF_FALSE(ienc->getAxes(&axes), "Cannot get number of controlled axes");
     std::string axisName;
@@ -109,7 +90,40 @@ bool Imu::setup(yarp::os::Property& property)
 
     ROBOTTESTINGFRAMEWORK_ASSERT_ERROR_IF_FALSE(MASclientDriver.open(MASclientOptions), "Unable to open the MAS client driver");
     ROBOTTESTINGFRAMEWORK_ASSERT_ERROR_IF_FALSE(MASclientDriver.isValid(), "Device driver cannot be opened");
-    ROBOTTESTINGFRAMEWORK_ASSERT_ERROR_IF_FALSE(MASclientDriver.view(iorientation), "Unable to open orientation interface");
+
+    yarp::os::Bottle & sensorsNamesList = sensorsList.addList();
+    yarp::os::Bottle *inputSensorsList;
+    inputSensorsList = property.find("sensorsList").asList();
+
+    if(inputSensorsList->size() == 0 || inputSensorsList->get(0).asString() == "all")
+    {
+        ROBOTTESTINGFRAMEWORK_TEST_REPORT("Testing all the IMUs available...");
+        sensorsNamesList.addString("l_arm_ft");
+        sensorsNamesList.addString("r_arm_ft");
+        sensorsNamesList.addString("head_imu_0");
+    }
+
+    else
+    {
+        ROBOTTESTINGFRAMEWORK_TEST_REPORT("Testing the list of provided IMUs...");
+        for(int sensorIndex = 0; sensorIndex < inputSensorsList->size(); sensorIndex++)
+        {
+            sensorsNamesList.addString(inputSensorsList->get(sensorIndex).asString());
+        }
+    }
+
+    yarp::os::Property MASremapperOptions;
+    MASremapperOptions.put("device", "multipleanalogsensorsremapper");
+    MASremapperOptions.put("OrientationSensorsNames", sensorsList.get(0));
+    ROBOTTESTINGFRAMEWORK_ASSERT_ERROR_IF_FALSE(MASremapperDriver.open(MASremapperOptions), "Unable to open the MAS remapper driver");
+    ROBOTTESTINGFRAMEWORK_ASSERT_ERROR_IF_FALSE(MASremapperDriver.isValid(), "Device driver cannot be opened");
+
+    ROBOTTESTINGFRAMEWORK_ASSERT_ERROR_IF_FALSE(MASremapperDriver.view(imultiwrap), "Unable to open multiple wrapper interface");
+
+    yarp::dev::PolyDriverList driverList;
+    driverList.push(&MASclientDriver, "alljoints_inertials");
+    ROBOTTESTINGFRAMEWORK_ASSERT_ERROR_IF_FALSE(imultiwrap->attachAll(driverList), "Unable to do the attach");
+    ROBOTTESTINGFRAMEWORK_ASSERT_ERROR_IF_FALSE(MASremapperDriver.view(iorientation), "Unable to open orientation interface");
         
     outputPort.open("/test-imu/out");
     ROBOTTESTINGFRAMEWORK_TEST_REPORT("Opening port "+outputPort.getName()+"...");
@@ -164,28 +178,85 @@ void Imu::tearDown()
 void Imu::run() 
 {
     ROBOTTESTINGFRAMEWORK_TEST_REPORT("Starting reading IMU orientation values...");
+    rpyValues.resize(sensorsList.get(0).asList()->size());
 
-    for (int sensorIndex = 0; sensorIndex < iorientation->getNrOfOrientationSensors(); sensorIndex++)
+    for (int sensorIndex = 0; sensorIndex < sensorsList.get(0).asList()->size(); sensorIndex++)
     {
-        iorientation->getOrientationSensorName(sensorIndex, sensorName);
-
-        ROBOTTESTINGFRAMEWORK_ASSERT_ERROR_IF_FALSE(iorientation->getOrientationSensorMeasureAsRollPitchYaw(sensorIndex, rpyValues, timestamp), "Unable to obtain rpy measurements.");
+        ROBOTTESTINGFRAMEWORK_ASSERT_ERROR_IF_FALSE(iorientation->getOrientationSensorName(sensorIndex, sensorName), "Unable to obtain rpy measurements.");
+        ROBOTTESTINGFRAMEWORK_ASSERT_ERROR_IF_FALSE(iorientation->getOrientationSensorMeasureAsRollPitchYaw(sensorIndex, rpyValues[sensorIndex], timestamp), "Unable to obtain rpy measurements.");
         iorientation->getOrientationSensorFrameName(sensorIndex, frameName);
         iDynTree::Rotation I_R_FK = kinDynComp.getWorldTransform(frameName).getRotation(); 
-        I_R_I_IMU = (I_R_FK * ((iDynTree::Rotation::RPY(iDynTree::deg2rad(rpyValues[0]), iDynTree::deg2rad(rpyValues[1]), iDynTree::deg2rad(rpyValues[2]))).inverse())); 
-    
-        double minLim;
-        double maxLim;
-
-        for (int movJoint = 0; movJoint < sensorMap[sensorName].size(); movJoint++)
-        {
-            ROBOTTESTINGFRAMEWORK_ASSERT_ERROR_IF_FALSE(ilim->getLimits(model.model().getJointIndex(sensorMap[sensorName].get(movJoint).toString()), &minLim, &maxLim), Asserter::format("Unable to get limits for joint %s", sensorMap[sensorName].get(movJoint).toString()));
-            moveJoint(model.model().getJointIndex(sensorMap[sensorName].get(movJoint).toString()), minLim + 5, sensorIndex);
-            yarp::os::Time::delay(1.);
-            moveJoint(model.model().getJointIndex(sensorMap[sensorName].get(movJoint).toString()), maxLim - 5, sensorIndex);
-            yarp::os::Time::delay(1.);
-        }
+        I_R_I_IMU = (I_R_FK * ((iDynTree::Rotation::RPY(iDynTree::deg2rad(rpyValues[sensorIndex][0]), iDynTree::deg2rad(rpyValues[sensorIndex][1]), iDynTree::deg2rad(rpyValues[sensorIndex][2]))).inverse()));    
     }
+
+    startMove();
+}
+
+bool Imu::startMove()
+{
+    bool done = false;
+    iDynTree::GeomVector3 error;
+    std::vector<std::vector<double>> errorTot(sensorsList.get(0).asList()->size());
+
+    yarp::os::Time::delay(.1);
+
+    while(!done)
+    {
+        for (int sensorIndex = 0; sensorIndex < sensorsList.get(0).asList()->size(); sensorIndex++)
+        {
+            sensorName = sensorsList.get(0).asList()->get(sensorIndex).asString();
+            iorientation->getOrientationSensorMeasureAsRollPitchYaw(sensorIndex, rpyValues[sensorIndex], timestamp);
+            iorientation->getOrientationSensorFrameName(sensorIndex, frameName);
+
+            ROBOTTESTINGFRAMEWORK_ASSERT_ERROR_IF_FALSE(ienc->getEncoders(positions.data()), "Cannot get joint positions");
+            ROBOTTESTINGFRAMEWORK_ASSERT_ERROR_IF_FALSE(ienc->getEncoderSpeeds(velocities.data()), "Cannot get joint velocities");
+
+            for (auto axIndex = 0; axIndex < axes; axIndex++)
+            {
+                s.setVal(axIndex, iDynTree::deg2rad(positions[axIndex]));
+                ds.setVal(axIndex, iDynTree::deg2rad(velocities[axIndex]));
+            }  
+
+            kinDynComp.setRobotState(
+            I_T_base,
+            s,
+            baseVelocity,
+            ds,
+            gravity);
+
+            iDynTree::Rotation expectedImuSignal = kinDynComp.getWorldTransform(frameName).getRotation();
+            iDynTree::Rotation imuSignal = (I_R_I_IMU * iDynTree::Rotation::RPY(iDynTree::deg2rad(rpyValues[sensorIndex][0]), iDynTree::deg2rad(rpyValues[sensorIndex][1]), iDynTree::deg2rad(rpyValues[sensorIndex][2]))); 
+            error = (expectedImuSignal * imuSignal.inverse()).log();
+
+            // sendData(expectedImuSignal.asRPY(), imuSignal.asRPY());
+
+            bufferManager.push_back(positions, "joints_state::positions");
+            bufferManager.push_back(velocities, "joints_state::velocities");
+            bufferManager.push_back({expectedImuSignal.asRPY()[0], expectedImuSignal.asRPY()[1], expectedImuSignal.asRPY()[2]}, "orientations::" + sensorName + "::expected");
+            bufferManager.push_back({imuSignal.asRPY()[0], imuSignal.asRPY()[1], imuSignal.asRPY()[2]}, "orientations::" + sensorName + "::measured");
+
+            double sumOfSquares = std::accumulate(error.begin(), error.end(), 0.0,
+            [](double accumulator, double element) {
+                return accumulator + element * element;
+            });
+
+            double mag = std::sqrt(sumOfSquares);
+            bufferManager.push_back(mag, "orientations::" + sensorName + "::error");
+
+            errorTot[sensorIndex].push_back(mag);
+        }
+
+        ipos->checkMotionDone(&done);
+    }
+
+    for(int sensorIndex = 0; sensorIndex < sensorsList.get(0).asList()->size(); sensorIndex++)
+    {
+        sensorName = sensorsList.get(0).asList()->get(sensorIndex).asString();
+        auto maxError = std::max_element(errorTot[sensorIndex].begin(), errorTot[sensorIndex].end());
+        ROBOTTESTINGFRAMEWORK_TEST_CHECK(*maxError < errorMean, Asserter::format("Testing sensor %s: the max rotation angle error is %f rad!", sensorName.c_str(), *maxError));
+    }
+
+    return true;
 }
 
 bool Imu::sendData(iDynTree::Vector3 expectedValues, iDynTree::Vector3 imuSignal)
@@ -212,69 +283,6 @@ bool Imu::sendData(iDynTree::Vector3 expectedValues, iDynTree::Vector3 imuSignal
     return true;
 }
 
-bool Imu::moveJoint(int ax, double pos, int sensorIndex)
-{
-    bool done = false;
-    iDynTree::GeomVector3 error;
-    std::vector<double> errorTot;
-    iorientation->getOrientationSensorName(sensorIndex, sensorName);
-    yarp::os::Time::delay(.1);
-
-    ipos->positionMove(ax, pos);
-
-    while(!done)
-    {
-        iorientation->getOrientationSensorMeasureAsRollPitchYaw(sensorIndex, rpyValues, timestamp);
-
-        ROBOTTESTINGFRAMEWORK_ASSERT_ERROR_IF_FALSE(ienc->getEncoders(positions.data()), "Cannot get joint positions");
-        ROBOTTESTINGFRAMEWORK_ASSERT_ERROR_IF_FALSE(ienc->getEncoderSpeeds(velocities.data()), "Cannot get joint velocities");
-
-        for (auto axIndex = 0; axIndex < axes; axIndex++)
-        {
-            s.setVal(axIndex, iDynTree::deg2rad(positions[axIndex]));
-            ds.setVal(axIndex, iDynTree::deg2rad(velocities[axIndex]));
-        }  
-
-        kinDynComp.setRobotState(
-        I_T_base,
-        s,
-        baseVelocity,
-        ds,
-        gravity);
-
-        iDynTree::Rotation expectedImuSignal = kinDynComp.getWorldTransform(frameName).getRotation();
-        iDynTree::Rotation imuSignal = (I_R_I_IMU * iDynTree::Rotation::RPY(iDynTree::deg2rad(rpyValues[0]), iDynTree::deg2rad(rpyValues[1]), iDynTree::deg2rad(rpyValues[2]))); 
-        error = (expectedImuSignal * imuSignal.inverse()).log();
-
-        errorTot.reserve(error.size());
-
-        double sumOfSquares = std::accumulate(error.begin(), error.end(), 0.0,
-        [](double accumulator, double element) {
-            return accumulator + element * element;
-        });
-    
-        double mag = std::sqrt(sumOfSquares);
-        errorTot.push_back(mag);
- 
-        sendData(expectedImuSignal.asRPY(), imuSignal.asRPY());
-
-        bufferManager.push_back(positions, "joints_state::positions");
-        bufferManager.push_back(velocities, "joints_state::velocities");
-        bufferManager.push_back({expectedImuSignal.asRPY()[0], expectedImuSignal.asRPY()[1], expectedImuSignal.asRPY()[2]}, "orientations::" + sensorName + "::expected");
-        bufferManager.push_back({imuSignal.asRPY()[0], imuSignal.asRPY()[1], imuSignal.asRPY()[2]}, "orientations::" + sensorName + "::measured");
-
-        ipos->checkMotionDone(&done);
-    }
-
-    double maxError = *max_element(errorTot.begin(), errorTot.end());
-    bufferManager.push_back(maxError, "orientations::" + sensorName + "::error::" + axesVec[ax]);
-
-    ROBOTTESTINGFRAMEWORK_TEST_CHECK(maxError < errorMean, Asserter::format("Testing %s for sensor %s: the max rotation angle error is %f rad!", axesVec[ax].c_str(), sensorName.c_str(), maxError));
-    ipos->positionMove(ax, 0.0);
-
-    return true;
-}
-
 bool Imu::setupTelemetry()
 {
     robometry::BufferConfig bufferConfig;
@@ -287,15 +295,11 @@ bool Imu::setupTelemetry()
     bufferManager.addChannel({"joints_state::positions", {axesVec.size(), 1}, axesVec});
     bufferManager.addChannel({"joints_state::velocities", {axesVec.size(), 1}, axesVec});
 
-    for(auto it = sensorMap.begin(); it != sensorMap.end(); it++) 
+    for(auto sensorIndex = 0; sensorIndex < sensorsList.get(0).asList()->size(); sensorIndex++) 
     {
-        bufferManager.addChannel({"orientations::" + it->first + "::expected", {3, 1}, {"r", "p", "y"}});
-        bufferManager.addChannel({"orientations::" + it->first + "::measured", {3, 1}, {"r", "p", "y"}});
-        
-        for(int i = 0; i < sensorMap[it->first].size(); i ++)
-        {
-            bufferManager.addChannel({"orientations::" + it->first + "::error::" + sensorMap[it->first].get(i).toString(), {1, 1}, {"max_error"}});
-        }
+        bufferManager.addChannel({"orientations::" + sensorsList.get(0).asList()->get(sensorIndex).asString() + "::expected", {3, 1}, {"r", "p", "y"}});
+        bufferManager.addChannel({"orientations::" + sensorsList.get(0).asList()->get(sensorIndex).asString() + "::measured", {3, 1}, {"r", "p", "y"}});
+        bufferManager.addChannel({"orientations::" + sensorsList.get(0).asList()->get(sensorIndex).asString() + "::error", {1, 1}, {"error"}});
     }
     
     return bufferManager.configure(bufferConfig);
